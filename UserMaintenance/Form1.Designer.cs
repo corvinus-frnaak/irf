375523eb5f05740284b28a93441b2482444a42a5
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-09-30 19:37:29 +0200

    Revert "1.2"
    This reverts commit 0be06815235e0be7b6ff15128f76bd0e659bdb60.

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -83,9 +83,8 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(138, 30);
 			this.button1.TabIndex = 5;
-			this.button1.Text = "Add";
+			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// Form1
 			// 
