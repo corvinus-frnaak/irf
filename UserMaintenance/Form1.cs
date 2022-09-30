bf5e96ea85cc146b6110490c25bb7d4c9c7fa690
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-09-30 20:31:38 +0200

    1.12

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -12,23 +12,9 @@ namespace UserMaintenance
 {
 	public partial class Form1 : Form
 	{
-		BindingList<Entities.User> users = new BindingList<Entities.User>();
 		public Form1()
 		{
 			InitializeComponent();
-			label1.Text = Resource1.FullName;
-			button2.Text = Resource1.WriteToFile;
-			button1.Text = Resource1.Add;
-		}
-
-		private void button2_Click(object sender, EventArgs e)
-		{
-			SaveFileDialog s = new SaveFileDialog();
-			if (s.ShowDialog()==DialogResult.OK)
-			{
-				
-			}
-			
 		}
 	}
 }
