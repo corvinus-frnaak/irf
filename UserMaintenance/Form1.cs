0095637cfcce6b04a75ee78b61b42fc16dd21bce
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-09-30 20:05:03 +0200

    1.7

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,24 @@ namespace UserMaintenance
 		public Form1()
 		{
 			InitializeComponent();
+			label1.Text = Resource1.FullName;
+			button2.Text = Resource1.WriteToFile;
+			button1.Text = Resource1.Add;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			using (var sfd = new SaveFileDialog())
+			{
+				sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+				sfd.FilterIndex = 2;
+
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					File.WriteAllText(sfd.FileName, textBox_ListDestination.Text);
+				}
+			}
+
 		}
 
 		private void button1_Click(object sender, EventArgs e)
