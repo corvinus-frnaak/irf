cdc1bc377fea2b3e4bbfae47e0523a624b5840c7
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-09-30 19:54:15 +0200

    1.5.3

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -12,6 +12,7 @@ namespace UserMaintenance
 {
 	public partial class Form1 : Form
 	{
+		BindingList<Entities.User> users = new BindingList<Entities.User>();
 		public Form1()
 		{
 			InitializeComponent();
