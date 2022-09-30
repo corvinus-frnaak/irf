225cb4a258d044f1fc37f131aacf805efeb2b12c
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-09-30 20:05:03 +0200

    1.7

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -12,7 +12,6 @@ using System.Windows.Forms;
 namespace UserMaintenance
 {
 	public partial class Form1 : Form
-
 	{
 		BindingList<Entities.User> users = new BindingList<Entities.User>();
 		public Form1()
@@ -21,9 +20,6 @@ namespace UserMaintenance
 			label1.Text = Resource1.FullName;
 			button2.Text = Resource1.WriteToFile;
 			button1.Text = Resource1.Add;
-			listBox1.DataSource = users;
-			listBox1.ValueMember = "ID";
-			listBox1.DisplayMember = "FullName";
 		}
 
 		private void button2_Click(object sender, EventArgs e)
@@ -31,33 +27,14 @@ namespace UserMaintenance
 			using (var sfd = new SaveFileDialog())
 			{
 				sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-				sfd.FilterIndex = 1;
+				sfd.FilterIndex = 2;
 
 				if (sfd.ShowDialog() == DialogResult.OK)
 				{
-					TextWriter tw = new StreamWriter(sfd.FileName);
-					for (int i = 0; i < users.Count; i++)
-					{
-						tw.WriteLine(users[i].ID);
-						tw.WriteLine(users[i].FullName);
-					}
-					
-					tw.Close();
-					
-
+					File.WriteAllText(sfd.FileName, textBox_ListDestination.Text);
 				}
 			}
 
-		}
-
-		private void button1_Click(object sender, EventArgs e)
-		{
-			var u = new Entities.User()
-			{
-				FullName = textBox1.Text,
-			};
-			users.Add(u);
-			textBox1.Text = "";
 		}
 
 		private void button1_Click(object sender, EventArgs e)
