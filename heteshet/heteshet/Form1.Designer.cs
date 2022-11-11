0fc3e0cd19b6d70e79d72ed3c2261fb4ed406373
Csaba Kristóf Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-11 14:43:50 +0100

    0.4

--- heteshet/heteshet/Form1.Designer.cs
+++ heteshet/heteshet/Form1.Designer.cs
@@ -31,31 +31,49 @@ namespace heteshet
 		{
 			this.dgwPortfolio = new System.Windows.Forms.DataGridView();
 			this.dgwTick = new System.Windows.Forms.DataGridView();
+			this.btSave = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dgwPortfolio)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.dgwTick)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dgwPortfolio
 			// 
+			this.dgwPortfolio.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgwPortfolio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dgwPortfolio.Location = new System.Drawing.Point(12, 12);
 			this.dgwPortfolio.Name = "dgwPortfolio";
-			this.dgwPortfolio.Size = new System.Drawing.Size(369, 302);
+			this.dgwPortfolio.Size = new System.Drawing.Size(369, 291);
 			this.dgwPortfolio.TabIndex = 0;
 			// 
 			// dgwTick
 			// 
+			this.dgwTick.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.dgwTick.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dgwTick.Location = new System.Drawing.Point(387, 12);
 			this.dgwTick.Name = "dgwTick";
-			this.dgwTick.Size = new System.Drawing.Size(369, 302);
+			this.dgwTick.Size = new System.Drawing.Size(369, 291);
 			this.dgwTick.TabIndex = 1;
+			// 
+			// btSave
+			// 
+			this.btSave.Location = new System.Drawing.Point(12, 331);
+			this.btSave.Name = "btSave";
+			this.btSave.Size = new System.Drawing.Size(118, 42);
+			this.btSave.TabIndex = 2;
+			this.btSave.Text = "Mentés";
+			this.btSave.UseVisualStyleBackColor = true;
+			this.btSave.Click += new System.EventHandler(this.btSave_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(800, 439);
+			this.Controls.Add(this.btSave);
 			this.Controls.Add(this.dgwTick);
 			this.Controls.Add(this.dgwPortfolio);
 			this.Name = "Form1";
@@ -70,6 +88,7 @@ namespace heteshet
 
 		private System.Windows.Forms.DataGridView dgwPortfolio;
 		private System.Windows.Forms.DataGridView dgwTick;
+		private System.Windows.Forms.Button btSave;
 	}
 }
 
