bde24b685ba2cb8635242b96d342c47889be8a32
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-13 14:51:29 +0100

    0.2

---
+++ otodikhet/Entities/RateData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otodikhet.Entities
+{
+	internal class RateData
+	{
+		public RateData()
+		{
+			DateTime Date;
+			String Currency;
+			Decimal Value;
+		}
+	}
+}
--- otodikhet/Form1.cs
+++ otodikhet/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using otodikhet.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,22 @@ namespace otodikhet
 {
 	public partial class Form1 : Form
 	{
+		MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+		public void WebServiceCall()
+		{
+			var request = new GetExchangeRatesRequestBody()
+			{
+				currencyNames = "EUR",
+				startDate = "2020-01-01",
+				endDate = "2020-06-30"
+			};
+			var response = mnbService.GetExchangeRates(request);
+			var result = response.GetExchangeRatesResult;
+		}
 		public Form1()
 		{
 			InitializeComponent();
+			WebServiceCall();
 		}
 	}
 }
