c88a182cf1ab165c65fd8a43cd6fb17700d8c366
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-15 12:06:14 +0100

    0.2

---
+++ 6_het_gyak/Entities/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_het_gyak.Entities
+{
+	public class Ball: Label
+	{
+		public Ball()
+		{
+			AutoSize = false;
+			Width = 50;
+			Height = 50;
+			Paint += Ball_Paint;
+		}
+		protected void DrawImage(Graphics e)
+		{
+			e.FillEllipse(new SolidBrush(Color.Blue) , 0, 0, Width, Height);
+		}
+
+		private void Ball_Paint(object sender, PaintEventArgs e)
+		{
+			DrawImage(e.Graphics);
+			throw new NotImplementedException();
+		}
+		public void MoveBall()
+		{
+			Left++;
+		}
+	}
+}
--- 6_het_gyak/Form1.cs
+++ 6_het_gyak/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _6_het_gyak.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,44 @@ namespace _6_het_gyak
 {
 	public partial class Form1 : Form
 	{
+		List<Ball> _balls = new List<Ball>();
+		public BallFactory Factory; //BallFactory típusú kifejtett propery
 		public Form1()
 		{
+			mainPanel.Width = Width;
+		
+			Factory = new BallFactory();
 			InitializeComponent();
+		}
+
+		private void createTimer_Tick(object sender, EventArgs e)
+		{
+			var newBall = Factory.CreateNew();
+			_balls.Add(newBall);
+			newBall.Left = -newBall.Width;
+			mainPanel.Controls.Add(newBall);
+			
+		}
+
+		private void conveyorTimer_Tick(object sender, EventArgs e)
+		{
+			var maxPosition = 0;
+			foreach (Ball b in _balls)
+			{
+				b.MoveBall();
+				if (b.Left>maxPosition)
+				{
+					maxPosition = b.Left;
+				}
+			}
+			
+			if(maxPosition >= 1000)
+			{
+				var firstElement = _balls[0];
+				_balls.Remove(firstElement);
+				mainPanel.Controls.Remove(firstElement);
+			}
+
 		}
 	}
 }
