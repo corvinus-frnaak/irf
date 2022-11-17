c8b2fdc22f67f884e7e9bd97873bf95aabe43aff
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-17 11:02:38 +0100

    0.8

---
+++ 6_het_gyak/Entities/Present.cs
@@ -0,0 +1,32 @@
+﻿using _6_het_gyak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_het_gyak.Entities
+{
+	public class Present : Toy
+	{
+		public Present(Color ribbon, Color box)
+		{
+			ribbonBrush = new SolidBrush(ribbon);
+			boxBrush = new SolidBrush(box);
+		}
+		public SolidBrush ribbonBrush { get; set; }
+		public SolidBrush boxBrush { get; set; }
+		protected override void DrawImage(Graphics e)
+		{
+			e.FillRectangle(boxBrush, 0, 0, Width, Height);
+			e.FillRectangle(ribbonBrush, Width*2/5, 0, Width*1/5, Height);
+			e.FillRectangle(ribbonBrush, 0, Height * 2 / 5, Width, Height*1/5);
+		}
+
+		public override string ToyType()
+		{
+			return "Present";
+		}
+	}
+}
---
+++ 6_het_gyak/Entities/PresentFactory.cs
@@ -0,0 +1,20 @@
+﻿using _6_het_gyak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_het_gyak.Entities
+{
+	public class PresentFactory : IToyFactory
+	{
+		public Color ribbon{ get; set; }
+		public Color box { get; set; }
+		public Toy CreateNew()
+		{
+			return new Present(ribbon,box);
+		}
+	}
+}
