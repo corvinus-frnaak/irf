815e2601110f3759442f48ebe6577683fb465a7c
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-17 11:19:13 +0100

    0.2

---
+++ zhgyak/Abstractions/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zhgyak.Abstractions
+{
+	public abstract class Product:Button
+	{
+		public Product()
+		{
+			Width = 150;
+			Height = 50;
+		}
+		public virtual string Title
+		{
+			set
+			{
+				Text = value;
+			}
+		}
+		public virtual int Calories { set { Display(); } }
+		public abstract void Display();
+
+	}
+}
---
+++ zhgyak/Entities/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zhgyak.Abstractions;
+
+namespace zhgyak.Entities
+{
+	internal class Food : Product
+	{
+		public override void Display()
+		{
+			
+		}
+	}
+}
