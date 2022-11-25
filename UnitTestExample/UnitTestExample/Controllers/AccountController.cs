60de2f35414e90c2552925d7f3626fdfb792d45e
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-11-25 14:56:10 +0100

    0.5

--- UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -18,23 +18,23 @@ namespace UnitTestExample.Controllers
         public AccountController()
         {
             AccountManager = new AccountManager();
-        }
+    }
 
         public Account Register(string email, string password)
         {
             if(!ValidateEmail(email))
-			{
+			  {
         throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
 
-      }
+        }
       if (!ValidatePassword(password))
-			{
+			  {
         throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
 
-      }
+        }
 
         var account = new Account()
             {
