1b7e1055e6fabff9dd122b510a565a6144337587
Csaba Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2022-12-01 13:23:19 +0100

    0.2

---
+++ kilencedikgyak/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilencedikgyak.Entities
+{
+	public class BirthProbability
+	{
+		public int BirthYear { get; set; }
+		public int NbrOfChildren { get; set; }
+		public bool Probability { get; set; }
+	}
+}
